611a8d6c2249967db590f3ea57853e350331e953
tomsar <tsarquis88@gmail.com>
2020-07-18 17:16:52 -0300

    fixes, avance

--- include/envioatr.h
+++ include/envioatr.h
@@ -6,3 +6,12 @@
 
 #define FILE_NAME_SIZE  32
 #define BUFFER_SIZE     16
+
+#define EXIT_MSG        "exit"
+
+using namespace std;
+
+void file_name_input    ( string *                  );
+void set_target_path    ( string *, string, size_t  );
+void exit_rutine        (                           );
+int move_file           ( string, string            );
--- src/envioatr.cpp
+++ src/envioatr.cpp
@@ -2,10 +2,6 @@
 
 using namespace std;
 
-void file_name_input( string * );
-int move_file( string, string );
-void set_target_path( string *, string, size_t );
-
 int
 main() 
 {
@@ -15,19 +11,26 @@ main()
     err = false;
 
     file_name_input( &source_path );
+    if( source_path.compare( EXIT_MSG ) == 0 )
+    {
+        exit_rutine();
+        return SUCCES;
+    }        
+
     set_target_path( &target_path, source_path, source_path.length());
 
     if( move_file( source_path, target_path ) == FAILURE )
     {
         cout << endl << "ERROR: something went wrong" << endl;;
         perror("");
-        cout << endl;
         err = true;      
     }
     else
     {
-        cout << endl << "File has been moved succesfully" << endl << endl;
+        cout << endl << "File has been moved succesfully" << endl;
     }
+
+    exit_rutine();
 
     if( err )
         return FAILURE;
@@ -38,12 +41,13 @@ main()
 void
 file_name_input( string *file_name )
 {
-    while( true )
+    cout << endl << "File name to transfer:" << endl;
+    
+    do
     {
-        cout << endl << "File name to transfer:" << endl << ">> ";
+        cout << ">> ";  
         getline( cin, *file_name );
-        break;
-    }
+    } while( file_name->empty() );
 }
 
 int
@@ -81,21 +85,28 @@ move_file( string source_path, string target_path )
 void
 set_target_path( string *target_path, string source_path, size_t len )
 {
-    int i, j, new_len;
-    string aux;
+    int i;
+    bool slash = false;
 
     for( i = (int)len - 1; i >= 0; i-- )
     {
         if( source_path[i] == '/' )
+        {
+            slash = true;
             break;
+        }
     }
 
-    new_len = (int)len - i;
+    if( slash )
+        target_path->assign( source_path, i, len - i);
+    else
+        target_path->assign( source_path );
 
-    for( j = 0; j < new_len; j++ )
-    {
-        aux[j] = source_path[ (i + 1) + j ];
-    }
+    *target_path = "./resources/" + *target_path;
+}
 
-    *target_path = aux;
+void
+exit_rutine()
+{
+    cout << endl << "Bye!" << endl << endl;
 }
