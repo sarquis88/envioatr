ff382982c270599735485b7999998fdf1a96ff75
tomsar <tsarquis88@gmail.com>
2020-07-19 11:11:47 -0300

    add libs

--- include/commons.h
+++ include/commons.h
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define SUCCES          0
 #define FAILURE         1
--- include/main.h
+++ include/main.h
@@ -1,6 +1,8 @@
 #include "commons.h"
 
 #include <unistd.h>
+#include <cerrno>
+#include <cstdio>
 
 #define SENDER_BIN      "./bin/sender.o"
 #define RECEIVER_BIN    "./bin/receiver.o"
